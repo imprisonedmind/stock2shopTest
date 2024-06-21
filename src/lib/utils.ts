c7595f76476eb50ace45fe794c0ee25253adefe5
Luke <luke.stephens@trintel.co.za>
2024-06-21 19:39:41 +0200

    fix missing attribute errors

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,23 +1,29 @@
-export const validateForm = (values) => {
+import { Product } from "@/app/types";
+
+export const validateForm = (values: Product) => {
   let errors: {
-    sku: string;
-    attributes: {
+    sku?: string;
+    attributes?: {
       size: string;
       grams: string;
     };
   } = {};
+
   if (!values.sku) {
     errors.sku = "Required";
   }
 
   if (!errors.attributes) {
-    errors.attributes = {};
+    errors.attributes = { grams: "", size: "" };
   }
 
   if (!values.attributes.size) {
     errors.attributes.size = "Required";
   }
-  if (parseInt(values.attributes.grams) <= 0 || !values.attributes.grams) {
+  if (
+    parseInt(values.attributes.grams as string) <= 0 ||
+    !values.attributes.grams
+  ) {
     errors.attributes.grams = "Must be greater than zero";
   }
   return errors;
